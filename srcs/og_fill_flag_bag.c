0028d94c0c56a7b6c930c8c93cbeaceb016d1d7c
oguzharunb <oguzharunb@gmail.com>
2024-10-10 13:01:03 +0300

    i wasnt used for iterating, bug fixed

--- srcs/og_fill_flag_bag.c
+++ srcs/og_fill_flag_bag.c
@@ -1,5 +1,7 @@
 #include "../includes/og_printf.h"
 
+// it expects a string with the flags to replace
+// the flag have to be in the order of FLAGS
 void og_fill_flag_bag(char const *to_replace, size_t size, t_flags *flags)
 {
 	size_t	i;
@@ -7,19 +9,18 @@ void og_fill_flag_bag(char const *to_replace, size_t size, t_flags *flags)
 	i = 0;
 	while (og_isin(FLAGS, *(to_replace + i)) && i < size)
 	{
-		if (*(to_replace) == FLAG_HASH)
+		if (*(to_replace + i) == FLAG_HASH)
 			flags->flag_hash = 1;
-		if (*(to_replace) == FLAG_MINUS)
+		else if (*(to_replace + i) == FLAG_MINUS)
 			flags->flag_min = 1;
-		if (*to_replace == FLAG_PLUS)
+		else if (*(to_replace + i) == FLAG_PLUS)
 			flags->flag_plus = 1;
-		if (*to_replace == FLAG_SPACE)
+		else if (*(to_replace + i) == FLAG_SPACE)
 			flags->flag_space = 1;
-		if (*to_replace == FLAG_ZERO)
+		else if (*(to_replace + i) == FLAG_ZERO)
 			flags->flag_zero = 1;
 		i++;
 	}
-
 	if ((*(to_replace + i) == DYN_WIDTH))
 	{
 		flags->dyn_width = 1;
@@ -44,7 +45,7 @@ void og_fill_flag_bag(char const *to_replace, size_t size, t_flags *flags)
 		}
 	}
 
-	while (og_isin(*(to_replace + i)) && i < size)
+	while (og_isin(LENGTH_MODIFIER, *(to_replace + i)) && i < size)
 	{
 		if (*(to_replace + i) == LM_SHORT)
 			flags->lm_short = flags->lm_short + 1;
@@ -52,6 +53,4 @@ void og_fill_flag_bag(char const *to_replace, size_t size, t_flags *flags)
 			flags->lm_long = flags->lm_long + 1;
 		i++;
 	}
-	if (og_isin(CONVERSIONS, *(to_replace + i)) && i < size)
-		return (1);
 }
