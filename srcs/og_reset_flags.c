0a0c192e1db478596caebe4e9057ecd1a735c23e
oguzharunb <oguzharunb@gmail.com>
2024-10-10 12:59:44 +0300

    og_reset_flags function made dynamic

--- srcs/og_reset_flags.c
+++ srcs/og_reset_flags.c
@@ -2,15 +2,5 @@
 
 void	og_reset_flags(t_flags *flags)
 {
-	flags->dyn_width = 0;
-	flags->flag_hash = 0;
-	flags->flag_min = 0;
-	flags->flag_plus = 0;
-	flags->flag_space = 0;
-	flags->flag_zero = 0;
-	flags->lm_long = 0;
-	flags->lm_short = 0;
-	flags->precision = 0;
-	flags->size = 0;
-	flags->width = 0;
+	*flags = (t_flags){ 0 };
 }
