b255a38cc9bfcf305046b1acb3687088d94cc889
oguzharunb <oguzharunb@gmail.com>
2024-10-11 04:39:47 +0300

    og_length_s and og_length_c added as functions

--- srcs/og_length.c
+++ srcs/og_length.c
@@ -2,10 +2,21 @@
 
 size_t og_length_s(t_flags *flags, char const *str)
 {
-	size_t	len;
+	int	len;
 
-	len = og_strlen(str);
+	len = ft_strlen(str);
 	if (flags->precision < len && flags->precision != -1)
 		len = flags->precision;
-	return (len);
+	if (flags->width > len)
+		return (flags->width);
+	return ((size_t)len);
 }
+
+size_t	og_length_c(t_flags *flags)
+{
+	if (flags->width)
+		return (flags->width);
+	return (1);
+}
+
+//size_t	og_length_d()
