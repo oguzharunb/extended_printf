ad9c538c72669ccabb2c877af584319225369a6a
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2024-10-12 18:25:06 +0300

    og_number_len_base, og_length_f implemented

--- srcs/og_length.c
+++ srcs/og_length.c
@@ -1,11 +1,30 @@
 #include "../includes/og_printf.h"
 #include <stdint.h>
 
+// '-' included
+size_t	og_number_len_base(int number, size_t base)
+{
+	size_t	len;
+	
+	len = 0;
+	if (number < 0)
+	{
+		len++;
+		number = -number;
+	}
+	while(number)
+	{
+		len++;
+		number /= base;
+	}
+	return (len);
+}
+
 size_t og_length_s(t_flags *flags, char const *str)
 {
 	int	len;
 
-	len = ft_strlen(str);
+	len = og_strlen(str);
 	if (flags->precision < len && flags->precision != -1)
 		len = flags->precision;
 	if (flags->width > len)
@@ -24,7 +43,7 @@ size_t	og_length_p(t_flags *flags, void *ptr)
 {
 	size_t		len;
 
-	len = 2 + ft_number_len_base((unsigned long)ptr, 16);
+	len = 2 + og_number_len_base((unsigned long)ptr, 16);
 	if ((size_t)flags->width > len)
 		return (flags->width);
 	return (len);
@@ -34,7 +53,7 @@ size_t	og_length_d(t_flags *flags, int	nbr)
 {
 	int	len;
 
-	len = ft_number_len_base(nbr, 10);
+	len = og_number_len_base(nbr, 10);
 	if (len > flags->width && len > flags->precision)
 		return (len);
 	if (flags->width > flags->precision)
@@ -46,7 +65,7 @@ size_t	og_length_o(t_flags *flags, int nbr)
 {
 	int	len;
 
-	len = ft_number_len_base(nbr, 8);
+	len = og_number_len_base(nbr, 8);
 	if (flags->flag_hash > 0)
 		len++;
 	if (len > flags->width && len > flags->precision)
@@ -54,4 +73,24 @@ size_t	og_length_o(t_flags *flags, int nbr)
 	if (flags->width > flags->precision)
 		return (flags->width);
 	return (flags->precision);
+}
+
+//printf("%d\n", ((number_i & 2139095040) >> 23) - 127); // 2139095040 = 0'1111111'00000000000000000000000 // that is exponent
+size_t	og_length_f(t_flags *flags, float number) //what if dyn_width is there
+{
+	unsigned int			number_i = (unsigned int)*(unsigned int *)&number;
+	register unsigned int	precision_wiss;
+	int						precision_len;
+	int						minus_one;
+	size_t					len;
+
+	minus_one = -1;
+	precision_wiss = 0;
+	while (number_i == (number_i & (minus_one << precision_wiss)))
+		precision_wiss++;
+	precision_len = 24 - precision_wiss - (int)(((number_i & 2139095040) >> 23) - 127); // (int)(((number_i & 2139095040) >> 23) - 127) is exponent
+	if (flags->precision > precision_len)
+		precision_len = flags->precision;
+	len = precision_len + 1 + (og_number_len_base((int)number, 10)); // minus included
+	return (len);
 }
