685e107e42bba714561425966963bbda6d4c5cd0
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2024-10-15 17:49:26 +0300

    tests for length_s and length_f implemented

--- tester/unit_tests/length_tests.c
+++ tester/unit_tests/length_tests.c
@@ -12,5 +12,46 @@ void	test_length_c()
 	og_fill_flag_bag(flag_str, ft_strlen(flag_str), &flags);
 	test_function(og_length_c(&flags) == 4, "Test 1: just width");
 
-	
+	og_reset_flags(&flags);
+	ft_strlcpy(flag_str, "-4", sizeof(flag_str));
+	og_fill_flag_bag(flag_str, ft_strlen(flag_str), &flags);
+	test_function(og_length_c(&flags) == 4, "Test 2: width and minus");
+}
+
+void	test_length_s()
+{
+	char	flag_str[100];
+	t_flags	flags;
+
+	og_reset_flags(&flags);
+	ft_strlcpy(flag_str, "4", sizeof(flag_str));
+	og_fill_flag_bag(flag_str, ft_strlen(flag_str), &flags);
+	test_function(og_length_s(&flags, "hello") == 5, "Test 1: len over width");
+
+	og_reset_flags(&flags);
+	ft_strlcpy(flag_str, ".4", sizeof(flag_str));
+	og_fill_flag_bag(flag_str, ft_strlen(flag_str), &flags);
+	test_function(og_length_s(&flags, "hello") == 4, "Test 2: precision over len");
+
+	og_reset_flags(&flags);
+	ft_strlcpy(flag_str, "7.4", sizeof(flag_str));
+	og_fill_flag_bag(flag_str, ft_strlen(flag_str), &flags);
+	test_function(og_length_s(&flags, "hello") == 7, "Test 3: width over precision over len");
+}
+
+void	test_length_f()
+{
+	char	flag_str[100];
+	t_flags	flags;
+
+	og_reset_flags(&flags);
+	ft_strlcpy(flag_str, "", sizeof(flag_str));
+	og_fill_flag_bag(flag_str, ft_strlen(flag_str), &flags);
+	test_function(og_length_f(&flags, 12.25) == 5, "Test 1: no flags");
+
+	og_reset_flags(&flags);
+	ft_strlcpy(flag_str, ".1", sizeof(flag_str));
+	og_fill_flag_bag(flag_str, ft_strlen(flag_str), &flags);
+	test_function(og_length_f(&flags, 12.25) == 4, "Test 2: precision limited");
+
 }
