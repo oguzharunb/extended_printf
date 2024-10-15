ab17b3436ff0258ea98ac126e8dffcb89111ab38
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2024-10-15 19:47:00 +0300

    new tests

--- tester/main.c
+++ tester/main.c
@@ -5,4 +5,7 @@
 int main(void)
 {
 	run_tests(test_length_c, "test_length_c");
+	run_tests(test_length_s, "test_length_s");
+	run_tests(test_length_f, "test_length_f");
+	run_tests(test_length_d, "test_length_d");
 }
