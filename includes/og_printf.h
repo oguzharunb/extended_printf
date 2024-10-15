86a155569fbd403831f415b33d165533880fadfb
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2024-10-15 19:45:50 +0300

    base and conversion added to format, macros defined as their standard format

--- includes/og_printf.h
+++ includes/og_printf.h
@@ -20,39 +20,41 @@
 # define S_FLAGS "- "
 # define C_FLAGS ""
 
-# define DOUBLE_EXPONENT_MASK 9218868437227405312
-# define FLOAT_EXPONENT_MASK 2139095040
-# define DOUBLE_PRECISION_MASK 4503599627370495
+# define DOUBLE_EXPONENT_MASK	0b0111111111110000000000000000000000000000000000000000000000000000//9218868437227405312
+# define DOUBLE_PRECISION_MASK	0b0000000000001111111111111111111111111111111111111111111111111111//4503599627370495
+# define FLOAT_EXPONENT_MASK	0b1111111100000000000000000000000//2139095040
 # include <stddef.h>
 
-typedef struct s_flags
+typedef struct s_format
 {
-	int	flag_min;
-	int	flag_plus;
-	int	flag_space;
-	int	flag_zero;
-	int	flag_hash;
-	int	lm_short;
-	int	lm_long;
-	int	dyn_width;
-	int	width;
-	int	precision;
-	int	size;
-} t_flags;
+	int		flag_min;
+	int		flag_plus;
+	int		flag_space;
+	int		flag_zero;
+	int		flag_hash;
+	int		lm_short;
+	int		lm_long;
+	int		dyn_width;
+	int		width;
+	int		precision;
+	int		size;
+	int		base;
+	char	conversion;
+} t_format;
 
-void	og_fill_flag_bag(char const *to_replace, size_t size, t_flags *flags);
-void	print_flags(t_flags *flags); // temporary
+void	og_fill_flag_bag(char const *to_replace, size_t size, t_format*flags);
+void	print_flags(t_format*flags); // temporary
 int		og_isin(char const *str, char c);
-void	og_reset_flags(t_flags *flags);
+void	og_reset_flags(t_format*flags);
 size_t	og_strlen(const char *s);
 size_t	og_number_len_base(long number, size_t base);
 
-size_t	og_length_o(t_flags *flags, int nbr);
-size_t	og_length_d(t_flags *flags, int	nbr);
-size_t	og_length_p(t_flags *flags, void *ptr);
-size_t	og_length_c(t_flags *flags);
-size_t 	og_length_s(t_flags *flags, char const *str);
-size_t	og_length_f(t_flags *flags, float number);
-size_t	og_length_e(t_flags *flags, double number);
-size_t	og_length_a(t_flags *flags, double number);
+size_t	og_length_o(t_format*flags, int nbr);
+size_t	og_length_d(t_format*flags, long nbr);
+size_t	og_length_p(t_format*flags, void *ptr);
+size_t	og_length_c(t_format*flags);
+size_t 	og_length_s(t_format*flags, char const *str);
+size_t	og_length_f(t_format*flags, float number);
+size_t	og_length_e(t_format*flags, double number);
+size_t	og_length_a(t_format*flags, double number);
 #endif
