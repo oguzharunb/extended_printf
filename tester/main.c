dd6720af0bec0ab01ddd755544184dea5790c761
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2024-10-15 20:01:21 +0300

    name changed

--- tester/main.c
+++ tester/main.c
@@ -7,5 +7,5 @@ int main(void)
 	run_tests(test_length_c, "test_length_c");
 	run_tests(test_length_s, "test_length_s");
 	run_tests(test_length_f, "test_length_f");
-	run_tests(test_length_d, "test_length_d");
+	run_tests(test_length_integer, "test_length_integer");
 }
