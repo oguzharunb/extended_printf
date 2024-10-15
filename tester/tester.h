77bca4611f1aade710276255ca8521747eddde35
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2024-10-15 17:23:04 +0300

    header for tester implemented

---
+++ tester/tester.h
@@ -0,0 +1,12 @@
+#ifndef TESTER_H
+# define TESTER_H
+
+# define RESET "\033[0m"
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+// prototypes
+void	test_function(int state, char *message);
+void	run_tests(void (*f)(void), const char *function_name);
+
+void	test_length_c();
+#endif
