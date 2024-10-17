8308a89097e86477b2bbc9172502a444969abd9d
oguzharunb <oguzharunb@gmail.com>
2024-10-17 17:35:31 +0300

    conversion 'a' added as define, 64 to 32 mask added. og_length_u added and og_unumber_len_base added as prototype

--- includes/og_printf.h
+++ includes/og_printf.h
@@ -1,6 +1,6 @@
 #ifndef OG_PRINTF_H
 # define OG_PRINTF_H
-# define CONVERSIONS "bscdeEfiouxX%"
+# define CONVERSIONS "bscdeaEfiouxX%"
 # define FLAGS "0#+- "
 # define LENGTH_MODIFIER "hl"
 # define NUMBERS "0123456789"
@@ -23,6 +23,7 @@
 # define DOUBLE_EXPONENT_MASK	0b0111111111110000000000000000000000000000000000000000000000000000//9218868437227405312
 # define DOUBLE_PRECISION_MASK	0b0000000000001111111111111111111111111111111111111111111111111111//4503599627370495
 # define FLOAT_EXPONENT_MASK	0b1111111100000000000000000000000//2139095040
+# define MASK_64_TO_32	0b0000000000000000000000000000000011111111111111111111111111111111 //4294967295
 # include <stddef.h>
 
 typedef struct s_format
@@ -47,9 +48,12 @@ void	print_flags(t_format*flags); // temporary
 int		og_isin(char const *str, char c);
 void	reset_flags(t_format*flags);
 size_t	og_strlen(const char *s);
-size_t	og_number_len_base(long number, size_t base);
 
-size_t	og_length_integer(t_format*flags, long nbr);
+size_t	og_number_len_base(long number, size_t base);
+size_t	og_unumber_len_base(unsigned long number, size_t base);
+
+size_t	og_length_i(t_format*flags, long nbr);
+size_t	og_length_u(t_format*flags, unsigned long nbr);
 size_t	og_length_p(t_format*flags, void *ptr);
 size_t	og_length_c(t_format*flags);
 size_t 	og_length_s(t_format*flags, char const *str);
