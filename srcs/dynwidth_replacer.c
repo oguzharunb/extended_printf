518298891ad4dc0c119b7d4e8c042bf406630040
oguzharunb <oguzharunb@gmail.com>
2024-10-29 12:16:06 +0300

    dyn_width to width

--- srcs/dynwidth_replacer.c
+++ srcs/dynwidth_replacer.c
@@ -4,6 +4,6 @@ int	replace_dynwidth(t_format *flags, va_list *args)
 {
 	if (!flags->dyn_width)
 		return (0);
-	flags->dyn_width = va_arg(*args, int);
+	flags->width = va_arg(*args, int);
 	return (1);
 }
