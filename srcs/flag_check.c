64a2c1b4269e0dde3da59c01844ba2f48531e497
oguzharunb <oguzharunb@gmail.com>
2024-10-29 11:39:55 +0300

    new check added

--- srcs/flag_check.c
+++ srcs/flag_check.c
@@ -1,6 +1,7 @@
 #include "../includes/og_printf.h"
+#include <unistd.h>
 
-#include <stdio.h> // temporary
+#include <stdio.h> //temporary
 int	flag_check(t_format *flags, size_t expected_size)
 {
 	size_t	size;
@@ -11,7 +12,12 @@ int	flag_check(t_format *flags, size_t expected_size)
 			+ flags->lm_long + flags->lm_short
 			+  og_unumber_len_base(flags->width, 10) + 1;
 	printf("size: %li, expected size: %li\n", size, expected_size);
-	if (size == expected_size)
-		return (size);
-	return (0);
+	if (flags->flag_hash && flags->flag_min)
+	{
+		write(STDOUT_FILENO, "# and - flags cannot be used together.\n", 39);
+		return (0);
+	}
+	if (size != expected_size)
+		return (0);
+	return (size);
 }
