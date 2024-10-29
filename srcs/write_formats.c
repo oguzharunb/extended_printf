08eea827f88f30d74cae6178f2a9da814a957c56
oguzharunb <oguzharunb@gmail.com>
2024-10-29 16:08:41 +0300

    function defines implemented

---
+++ srcs/write_formats.c
@@ -0,0 +1,59 @@
+#include "../includes/og_printf.h"
+#include "../libft/libft.h"
+
+int	write_signed_number(t_format *flags, char *dest, long number)
+{
+	(void)flags;
+	(void)dest;
+	(void)number;
+	return (1);
+}
+
+int	write_unsigned_number(t_format *flags, char *dest, unsigned long number)
+{
+	(void)flags;
+	(void)dest;
+	(void)number;
+	return (1);
+}
+
+int	write_string(t_format *flags, char *dest, char *src)
+{
+	(void)flags;
+	(void)dest;
+	(void)src;
+	return (1);
+}
+
+int	write_pointer(t_format *flags, char *dest, void *address)
+{
+	(void)flags;
+	(void)dest;
+	(void)address;
+	return (1);
+}
+
+int	write_double(t_format *flags, char *dest, double number)
+{
+	(void)flags;
+	(void)dest;
+	(void)number;
+	return (1);
+}
+
+int	write_scientific(t_format *flags, char *dest, double number)
+{
+	(void)flags;
+	(void)dest;
+	(void)number;
+	return (1);
+}
+
+int write_float(t_format *flags, char *dest, float number)
+{
+	(void)flags;
+	(void)dest;
+	(void)number;
+	return (1);
+}
+
