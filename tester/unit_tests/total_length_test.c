6fb108f7b812e8219e24683898f25c0623c6407f
oguzharunb <oguzharunb@gmail.com>
2024-11-02 05:40:09 +0300

    some float tests

--- tester/unit_tests/total_length_test.c
+++ tester/unit_tests/total_length_test.c
@@ -31,4 +31,24 @@ void	test_length_total()
 	ret = og_printf("%f", 1.0625f);
 	printf("ret: %d\n", ret);
 	test_function(ret == 6, "Test 8: float");
+
+	ret = og_printf("%f", 0.5f);
+	printf("ret: %d\n", ret);
+	test_function(ret == 3, "Test 9: float < 1");
+
+	ret = og_printf("%f", 0.25f);
+	printf("ret: %d\n", ret);
+	test_function(ret == 4, "Test 9: float < 1");
+
+	ret = og_printf("%f", 0.125f);
+	printf("ret: %d\n", ret);
+	test_function(ret == 5, "Test 9: float < 1");
+
+	ret = og_printf("%f", 0.0625f);
+	printf("ret: %d\n", ret);
+	test_function(ret == 6, "Test 9: float < 1");
+	
+	ret = og_printf("%f", 0.03125f);
+	printf("ret: %d\n", ret);
+	test_function(ret == 7, "Test 9: float < 1");
 }
