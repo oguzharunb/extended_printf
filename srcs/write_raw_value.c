757eb1a2822b1c0f02f2feae0c4ed5652100531a
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2024-11-04 23:10:09 +0300

    write_signed_function implemented

--- srcs/write_raw_value.c
+++ srcs/write_raw_value.c
@@ -1,23 +1,42 @@
-#include "includes/og_printf.h"
+#include "../includes/og_printf.h"
 
-//void	write_signed_number(long number, char *dest, t_format *flags, size_t digits)
-//{
-//	long	num;
-//	size_t	i;
-//
-//	i = 0;
-//	num = number;
-//	while (i < flags->width)
-//	{
-//		if (num / 10)
-//			*dest
-//	}
-//	num = number;
-//	*(dest + digits) = 0;
-//	while (digits--)
-//	{
-//		*(dest + digits) = (num % 10) + '0';
-//		num /= 10;
-//	}
-//}
-//
+void	write_signed_number(long number, char *dest, t_format *flags)
+{
+	int i;
+	int filler;
+	int tmp;
+
+	tmp = number;
+	i = 0;
+	if (flags->flag_zero)
+		filler = '0';
+	else
+		filler = ' ';
+	while (i < flags->width)
+		dest[i++] = filler;
+	if (i < (int)og_number_len_base(tmp, 10))
+		i = (int)og_number_len_base(tmp, 10);
+	if (number < 0)
+		number = -number;
+	if (flags->flag_min)
+		i = og_number_len_base(tmp, 10);
+	while (number)
+	{
+		dest[--i] = (number % 10) + '0';
+		number /= 10;
+	}
+	if (tmp < 0)
+	{
+        if (flags->flag_zero)
+            *dest = '-';
+        else
+            dest[i - 1] = '-';
+    }
+    else if (flags->flag_plus)
+    {
+		if (flags->flag_plus)
+			*dest = '+';
+		else
+			dest[i - 1] = '+';  
+	}
+}
