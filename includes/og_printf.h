c1ec281c1a8493fb3e197dfdf3f1527b974a2970
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2024-11-07 01:48:07 +0300

    FRACTION_ITER for iterating bit by bit

--- includes/og_printf.h
+++ includes/og_printf.h
@@ -22,8 +22,10 @@
 
 # define DOUBLE_EXPONENT_MASK	0b0111111111110000000000000000000000000000000000000000000000000000//9218868437227405312
 # define DOUBLE_PRECISION_MASK	0b0000000000001111111111111111111111111111111111111111111111111111//4503599627370495
-# define FLOAT_EXPONENT_MASK	0b1111111100000000000000000000000//2139095040
-# define MASK_64_TO_32	0b0000000000000000000000000000000011111111111111111111111111111111 //4294967295
+# define FLOAT_EXPONENT_MASK	0b01111111100000000000000000000000//2139095040
+# define MASK_64_TO_32			0b0000000000000000000000000000000011111111111111111111111111111111 //4294967295
+# define FLOAT_FRACTION_MASK	0b00000000011111111111111111111111
+# define FRACTION_ITER			0b00000000010000000000000000000000
 # include <stddef.h>
 # include <stdarg.h>
 
@@ -55,6 +57,8 @@ unsigned long	cast_unumber(t_format *flags, unsigned long nbr);
 long			cast_number(t_format *flags, long nbr);
 size_t			og_unumber_len_base(unsigned long number, size_t base);
 size_t			og_number_len_base(long number, size_t base);
+long			power(long number, unsigned long power);
+long			ft_abs(long number);
 
 size_t			og_length_a(t_format*flags, double number);
 size_t			og_length_e(t_format*flags, double number);
@@ -71,7 +75,11 @@ size_t			lengthf(char const *string, va_list *args);
 int				flag_check(t_format *flags, size_t size);
 int				replace_dynwidth(t_format *flags, va_list *args);
 
+unsigned long	float_fraction(float number, long i);
+
 void			write_signed_number(long number, char *dest, t_format *flags);
+void			write_unsigned_number(unsigned long, char *dest, t_format *flags);
+void			write_float_number(float number, char *dest, t_format *flags);
 
 int				og_printf(const char *string, ...);
 #endif
