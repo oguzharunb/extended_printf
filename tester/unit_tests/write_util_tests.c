2edb26e0224c5cfda3ac1a6dd9b33efea0b29124
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2024-11-08 01:38:29 +0300

    util test for float revised

--- tester/unit_tests/write_util_tests.c
+++ tester/unit_tests/write_util_tests.c
@@ -6,8 +6,12 @@
 #include <stdio.h>
 void	write_util_tests(void)
 {
-	unsigned long ret;
+	unsigned long	ret;
+	long			basamak = 0;
+	ret = float_fraction(0.25, 1, 0, &basamak, 5);
+	test_function(ret == 25 && basamak == 2, "Test 1: 0.25");
 
-	ret = float_fraction(0.25, 1);
-	test_function(ret = 25, "Test 1: 0.25");
+	basamak = 0;
+	ret = float_fraction(0.75, 1, 0, &basamak, 5);
+	test_function(ret == 75 && basamak == 2, "Test 2: 0.75");
 }
