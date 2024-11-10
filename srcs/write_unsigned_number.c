08e80395c13523f080a810c8723723473413a55b
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2024-11-10 16:28:26 +0300

    unsigned number writing is complete

--- srcs/write_unsigned_number.c
+++ srcs/write_unsigned_number.c
@@ -1,12 +1,30 @@
 #include "../includes/og_printf.h"
-
-void	write_usigned_number2(unsigned long number, char *dest, t_format *flags, int i, unsigned long tmp)
+#include "../libft/libft.h"
+// -, #
+// if 0 # is overlapped
+// 0x123    
+// # is not for u
+static void	write_usigned_number2(unsigned long number, char *dest, t_format *flags, int i)
 {
-	(void)number;
-	(void)dest;
-	(void)flags;
-	(void)i;
-	(void)tmp;
+	char	str[17];
+	
+	ft_strlcpy(str, "0123456789abcdef", sizeof(str));
+	if (flags->conversion == 'X')
+		ft_strlcpy(str, "0123456789ABCDEF", sizeof(str));
+	while (number)
+	{
+		dest[--i] = str[number % flags->base];
+		number /= flags->base;
+	}
+	if (flags->flag_hash && !flags->flag_zero)
+	{
+		if (flags->conversion == 'o')
+			dest[--i] = '0';
+		else if (flags->conversion == 'X')
+			ft_memcpy(dest + i - 2, "0X", 2);
+		else if (flags->conversion == 'x')
+			ft_memcpy(dest + i - 2, "0x", 2);
+	}
 }
 
 void	write_usigned_number(unsigned long number, char *dest, t_format *flags)
@@ -23,9 +41,12 @@ void	write_usigned_number(unsigned long number, char *dest, t_format *flags)
 		filler = ' ';
 	while (i < flags->width)
 		dest[i++] = filler;
+	if (flags->flag_min)
+		i = 0;
 	if (i < (int)og_unumber_len_base(tmp, flags->base))
 		i = (int)og_unumber_len_base(tmp, flags->base);
-	if (flags->flag_min)
-		i = og_unumber_len_base(tmp, flags->base);
-	write_usigned_number2(number, dest, flags, i, tmp);
+	if ((int)og_unumber_len_base(tmp, flags->base) + flags->hash_len >= i
+		&& !flags->flag_zero && flags->flag_hash && flags->conversion != 'u')
+		i += flags->hash_len;
+	write_usigned_number2(number, dest, flags, i);
 }
