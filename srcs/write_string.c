20b17503f2a6f1f97bb331a372d4b7bc7c464d43
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2024-11-13 14:49:34 +0300

    string write implementation

---
+++ srcs/write_string.c
@@ -0,0 +1,27 @@
+#include "../includes/og_printf.h"
+#include "../libft/libft.h"
+
+// 'minus', 'width', 'precision'
+
+// "hello    ", "     hello", "   hel", "hel    "
+void	write_string(char *string, char *dest, t_format *flags)
+{
+	unsigned long	i;
+	unsigned long	start;
+	unsigned long	string_len;
+
+	i = 0;
+	ft_memset(dest, ' ', flags->width);
+	if (flags->precision >= 0)
+		string_len = flags->precision;
+	else
+		string_len = ft_strlen(string);
+	start = 0;
+	if (flags->flag_min)
+		start = flags->width - string_len;
+	while (string[i])
+	{
+		dest[i + start] = string[i];
+		i++;
+	}
+}
