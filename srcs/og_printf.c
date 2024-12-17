b866017c3807c5f215c517b54135e7a8cf515889
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2024-12-17 15:45:28 +0300

    printf test function

--- srcs/og_printf.c
+++ srcs/og_printf.c
@@ -1,19 +1,22 @@
 #include "../includes/og_printf.h"
+#include "../libft/libft.h"
 #include <stdarg.h>
 #include <stdlib.h>
 
+#include <unistd.h>
 int	og_printf(const char *string, ...)
 {
 	va_list	args;
-	size_t	len;
+	//size_t	len;
 	char	*final_string;
 
-	va_start(args, string);
-	len = lengthf(string, &args);
-	final_string = malloc(len + 1);
+	//va_start(args, string);
+	//len = lengthf(string, &args);
+	final_string = calloc(50, 1);
 	if (!final_string)
 		return (0);
 	va_start(args, string);
-	//fill_string(string, args, final_string);
-	return (int)len;
+	fill_string(string, &args, final_string);
+	write(1, final_string, ft_strlen(final_string));
+	return ft_strlen(final_string);
 }
